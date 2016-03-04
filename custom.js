92caaa9f5b754725836def472b2fd1532b5b990e
Sergio Rovira <sgr0691@users.noreply.github.com>
2016-03-04 02:25:30 -0500

    custom.js

---
+++ custom.js
@@ -0,0 +1,49 @@
+$(function () {
+
+    function initMap() {
+
+        var location = new google.maps.LatLng(38.895, -77.0366667);
+
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+            center: location,
+            zoom: 16,
+            panControl: false,
+            scrollwheel: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+
+        var markerImage = 'marker2.png';
+
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            icon: markerImage
+        });
+
+        var contentString = '<div class="info-window">' +
+                '<h3>The National Mall</h3>' +
+                '<div class="info-content">' +
+                '<p>The National Mall is home to ten of the museums of the Smithsonian Institution offering a variety of exhibits ranging from art to space exploration. Other major attractions include the national monuments and memorials, the U.S. Capitol Building, the National Gallery of Art and the U.S. Botanic Garden. The Mall is a national park with landscaped gardens and expansive open spaces that are often used for public events.</p>' +
+                '</div>' +
+                '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 500
+        });
+
+        marker.addListener('click', function () {
+            infowindow.open(map, marker);
+        });
+
+        var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
+
+        map.set('styles', styles);
+
+
+    }
+
+    google.maps.event.addDomListener(window, 'load', initMap);
+});
